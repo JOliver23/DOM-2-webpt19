063cfbdefbdb1bce4b2d240cc1fcbad50d2382e1
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-07-12 15:26:54 -0700

    bus pic transform mouseenter 2/10; mouseleave 3/10

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@ funTitle.addEventListener('mouseover', () => {
     funTitle.style.color = "red"
 });
 
+const busPic = document.querySelector(".intro img");
+busPic.addEventListener('mouseenter', () => {
+    busPic.style.transform = 'scale(1.3)'
+    busPic.style.transition = 'all 0.3s'
+})
+busPic.addEventListener('mouseleave', () => {
+    busPic.style.transform = 'scale(1)'
+})
