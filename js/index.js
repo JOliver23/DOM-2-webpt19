621f5b56754036d95e49c4426d1b9936de862215
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-07-12 15:33:46 -0700

    lets go pic drag effect 4/10

--- js/index.js
+++ js/index.js
@@ -11,3 +11,8 @@ busPic.addEventListener('mouseenter', () => {
 busPic.addEventListener('mouseleave', () => {
     busPic.style.transform = 'scale(1)'
 })
+
+const mapPic = document.querySelector(".content-section .img-content img");
+mapPic.addEventListener('drag', () => {
+    mapPic.src = "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+})
